3d49c3a73bdb08c7c8d05e9fd3f56ba0bef528d6
Vlad Gorbich <vladgorbich@gmail.com>
2017-03-14 15:47:15 -0500

    add basic website functionality

---
+++ src/main/java/com/gorbich/beta/web/controllers/AboutController.java
@@ -0,0 +1,13 @@
+package com.gorbich.beta.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AboutController {
+	
+	@RequestMapping("/about")
+	String about() {
+		return "about/about";
+	}
+}
--- src/test/java/com/gorbich/beta/BetaApplicationTests.java
+++ src/test/java/com/gorbich/beta/BetaApplicationTests.java
@@ -2,15 +2,29 @@ package com.gorbich.beta;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.gorbich.beta.web.i18n.I18nService;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Locale;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BetaApplicationTests {
-
+	
+	@Autowired
+	private I18nService i18NService;
+	
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService() throws Exception {
+		String expected = "Site avec fonctionnalité de connexion";
+		String messageId = "index.jumbotron.h1";
+		Locale locale = new Locale("fr");
+		String actual = i18NService.getMessage(messageId, locale);
+		assertThat(actual).isEqualTo(expected);
 	}
 
 }
